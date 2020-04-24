003a6292221947e6a576bb8c06944876c810283a
Edmarie Rivera <edmarierivera@Edmaries-MacBook.local>
2020-04-23 22:18:49 -0500

    final

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "89d9c63228dc91659679aaceda16c499"
+weather_api_key = "35a2fe42f7de39aa581e67b1cb8cf77b"
 
 # Google API Key
 g_key = "AIzaSyDK_tNh72h9nc-B6_0k5JdeUPRn2PmvoQo"
